aac82c4989bb7ffeeb279e22282616b6c60e2443
Zeredas <wondighizt2@gmail.com>
2025-12-14 22:47:44 +0800

    Added: Sounds for the game

--- mathCatcher/src/com/mathcatcher/Main.java
+++ mathCatcher/src/com/mathcatcher/Main.java
@@ -1,10 +1,14 @@
 package com.mathcatcher;
 
 import com.mathcatcher.game.GameWindow;
+import com.mathcatcher.utils.SoundManager;
 import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
+        // Initialize sound system
+        SoundManager.init();
+
         SwingUtilities.invokeLater(() -> {
             new GameWindow();
         });
